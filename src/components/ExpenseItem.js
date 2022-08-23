7859d349be5e40ead5071a2c6bf4eb2ff977225b
Odell Purifoy <opurifoy@springhealth.com>
2022-08-23 19:16:27 -0400

    Added constant data to component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
-import './ExpenseItem.css'
+import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2023, 2, 28);
+  const expenseCareInsurance = 'Car Insurance';
+  const expenseAmount = 294.67
+
   return (
     <div className='expense-item'>
-        <div>August 23rd 2022</div>
+        <div>{expenseDate.toISOString()}</div>
         <div className='expense-item__description'>
-            <h2>Car Insurance</h2>
-            <div className='expense-item__price'>$294.76</div>
+            <h2>{expenseCareInsurance}</h2>
+            <div className='expense-item__price'>${expenseAmount}</div>
         </div>
     </div>
   )
