fbef6e280e73500b3c62443ce668548deab562a0
Odell Purifoy <opurifoy@springhealth.com>
2023-01-04 15:57:42 -0500

    Conditionally render expenses or no expenses found message

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -19,14 +19,18 @@ const Expenses = (props) => {
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onFilterChangeHandler={filterChangeHandler} />
-        {filteredExpenses.map((expense => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title} 
-            amount={expense.amount} 
-            date={expense.date}
-          />
-        )))}
+        {filteredExpenses.length === 0 ? (
+          <p>No Expenses Found</p>
+          ) : (
+            filteredExpenses.map((expense => (
+              <ExpenseItem
+                key={expense.id}
+                title={expense.title} 
+                amount={expense.amount} 
+                date={expense.date}
+              />
+             ))
+          ))}
       </Card>
     </div>
   );
