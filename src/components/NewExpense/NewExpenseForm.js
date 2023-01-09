ea5b94ff736416925e9014f30d0fa370ad8a3d07
Odell Purifoy <opurifoy@springhealth.com>
2023-01-09 14:44:31 -0500

    Got toggle form to work

--- src/components/NewExpense/NewExpenseForm.js
+++ src/components/NewExpense/NewExpenseForm.js
@@ -5,6 +5,7 @@ const NewExpenseForm = (props) => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
+    const [showForm, setShowForm] = useState(true)
 
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value)
@@ -33,28 +34,35 @@ const NewExpenseForm = (props) => {
         setEnteredDate('')
     }
 
-    return(
-        <form onSubmit={submitHandler}>
-            <div className='new-expense__controls'>
-                <div className='new-expense__control'>
-                    <label>Title</label>
-                    <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
+    const toggleHandler = (event) => {
+        setShowForm(false);
+    }
+
+    if (showForm) {
+        return ( <button onClick={toggleHandler}> Add New Expense </button> ) 
+    } else {
+        return(
+            <form onSubmit={submitHandler}>
+                <div className='new-expense__controls'>
+                    <div className='new-expense__control'>
+                        <label>Title</label>
+                        <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
+                    </div>
+                    <div className='new-expense__control'>
+                        <label>Amount</label>
+                        <input type='number' min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler} />
+                    </div>
+                    <div className='new-expense__control'>
+                        <label>Date</label>
+                        <input type='date' min="2022-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler} />
+                    </div>
                 </div>
-                <div className='new-expense__control'>
-                    <label>Amount</label>
-                    <input type='number' min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler} />
+                <div className='new-expense__actions'>
+                    <button>Cancel</button>
+                    <button type='submit'>Add Expense</button>
                 </div>
-                <div className='new-expense__control'>
-                    <label>Date</label>
-                    <input type='date' min="2022-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler} />
-                </div>
-            </div>
-            <div className='new-expense__actions'>
-                <button>Cancel</button>
-                <button type='submit'>Add Expense</button>
-            </div>
-        </form>
-    )
-}
+            </form>)
+        }
+    }
 
 export default NewExpenseForm;
